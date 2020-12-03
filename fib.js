ef03468524fb420aa00628252745e281a0eb6b45
altmattr <alt.mattr@gmail.com>
2020-12-03 12:14:51 +0000

    added a task to make an infinite version.  Still haven't finished the node runner experiment.

--- fib.js
+++ fib.js
@@ -2,9 +2,13 @@
  
         });
       
+fib_forever = Rx.Observable.create(subscriber => {
+
+})
 mocha.setup("bdd");
 
-describe("fibonacci", () => {
+describe("fast fibonacci", function(){
+  this.timeout(500);
   it("should emit at least ten elements", (done) => {
     fib.take(10)
         .count()
@@ -38,4 +42,38 @@ describe("fibonacci", () => {
   })
 });
 
+describe("forever fibonacci", function(){
+  this.timeout(30000);
+  it("should emit at least ten elements", (done) => {
+    fib_forever.take(10)
+               .count()
+               .subscribe(v => {
+                 chai.assert.equal(v,10);
+                 done();
+               });
+  });
+  it("should start with 0", (done) => {
+    fib_forever.first()
+        .subscribe(v => {
+          chai.assert.equal(v, 0);
+          done();
+        });
+  });
+  it("should have a 1 in the second slot", (done) => {
+    fib_forever.skip(1)
+        .first()
+        .subscribe(v => {
+          chai.assert.equal(v, 1);
+          done();
+        });
+  });
+  it("the 50th value should be correct", (done) => {
+    fib_forever.skip(49)
+        .first()
+        .subscribe(v => {
+          chai.assert.equal(v, 7778742049);
+          done();
+        });
+  })
+});
 mocha.run();
