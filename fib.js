5b575e967a296febda61a7045943f3fa2e0c3fd6
altmattr <alt.mattr@gmail.com>
2020-12-04 01:06:47 +0000

    adjustments to separate the testing

--- fib.js
+++ fib.js
@@ -6,75 +6,74 @@ fib_forever = Rx.Observable.create(subscriber => {
 
 });
 
-mocha.setup("bdd");
+function tests(){
+  describe("fast fibonacci", function(){
+    this.timeout(500);
+    it("should emit at least ten elements", (done) => {
+      fib.take(10)
+          .count()
+          .subscribe(v => {
+            chai.assert.equal(v,10);
+            done();
+          });
+    });
+    it("should start with 0", (done) => {
+      fib.first()
+          .subscribe(v => {
+            chai.assert.equal(v, 0);
+            done();
+          });
+    });
+    it("should have a 1 in the second slot", (done) => {
+      fib.skip(1)
+          .first()
+          .subscribe(v => {
+            chai.assert.equal(v, 1);
+            done();
+          });
+    });
+    it("the 50th value should be correct", (done) => {
+      fib.skip(49)
+          .first()
+          .subscribe(v => {
+            chai.assert.equal(v, 7778742049);
+            done();
+          });
+    })
+  });
 
-describe("fast fibonacci", function(){
-  this.timeout(500);
-  it("should emit at least ten elements", (done) => {
-    fib.take(10)
-        .count()
-        .subscribe(v => {
-          chai.assert.equal(v,10);
-          done();
-        });
+  describe("forever fibonacci", function(){
+    this.timeout(30000);
+    it("should emit at least ten elements", (done) => {
+      fib_forever.take(10)
+                .count()
+                .subscribe(v => {
+                  chai.assert.equal(v,10);
+                  done();
+                });
+    });
+    it("should start with 0", (done) => {
+      fib_forever.first()
+          .subscribe(v => {
+            chai.assert.equal(v, 0);
+            done();
+          });
+    });
+    it("should have a 1 in the second slot", (done) => {
+      fib_forever.skip(1)
+          .first()
+          .subscribe(v => {
+            chai.assert.equal(v, 1);
+            done();
+          });
+    });
+    it("the 50th value should be correct", (done) => {
+      fib_forever.skip(49)
+          .first()
+          .subscribe(v => {
+            chai.assert.equal(v, 7778742049);
+            done();
+          });
+    })
   });
-  it("should start with 0", (done) => {
-    fib.first()
-        .subscribe(v => {
-          chai.assert.equal(v, 0);
-          done();
-        });
-  });
-  it("should have a 1 in the second slot", (done) => {
-    fib.skip(1)
-        .first()
-        .subscribe(v => {
-          chai.assert.equal(v, 1);
-          done();
-        });
-  });
-  it("the 50th value should be correct", (done) => {
-    fib.skip(49)
-        .first()
-        .subscribe(v => {
-          chai.assert.equal(v, 7778742049);
-          done();
-        });
-  })
-});
-
-describe("forever fibonacci", function(){
-  this.timeout(30000);
-  it("should emit at least ten elements", (done) => {
-    fib_forever.take(10)
-               .count()
-               .subscribe(v => {
-                 chai.assert.equal(v,10);
-                 done();
-               });
-  });
-  it("should start with 0", (done) => {
-    fib_forever.first()
-        .subscribe(v => {
-          chai.assert.equal(v, 0);
-          done();
-        });
-  });
-  it("should have a 1 in the second slot", (done) => {
-    fib_forever.skip(1)
-        .first()
-        .subscribe(v => {
-          chai.assert.equal(v, 1);
-          done();
-        });
-  });
-  it("the 50th value should be correct", (done) => {
-    fib_forever.skip(49)
-        .first()
-        .subscribe(v => {
-          chai.assert.equal(v, 7778742049);
-          done();
-        });
-  })
-});
-mocha.run();
+}
